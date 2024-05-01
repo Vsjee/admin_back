8376c07788a4cf1ccc90d5b29d4abeae64e12c0d
David Hernandez <davidfelipehernandez1@gmail.com>
2024-05-01 09:14:52 -0500

    remove: account router

--- app/api.ts
+++ app/api.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 
 import customersRouter from './customers/infrastructure/api/customers-router';
-import accountsRouter from './account/infrastructure/api/accounts-router';
 import kidsRouter from './kids/infrastructure/api/kids-router';
 import booksRouter from './books/infrastructure/api/books-router';
 import storiesRouter from './story/infraestructure/api/story-router';
@@ -9,7 +8,6 @@ import storiesRouter from './story/infraestructure/api/story-router';
 const router = express.Router();
 
 router.use('/customers', customersRouter);
-router.use('/accounts', accountsRouter);
 router.use('/kids', kidsRouter);
 router.use('/books', booksRouter);
 router.use('/stories', storiesRouter);

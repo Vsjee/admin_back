1d22bb658a484345014164b4a7dc2da5488e99f5
David Hernandez <davidfelipehernandez1@gmail.com>
2024-05-01 08:43:39 -0500

    add: stories routing

--- app/api.ts
+++ app/api.ts
@@ -4,6 +4,7 @@ import customersRouter from './customers/infrastructure/api/customers-router';
 import accountsRouter from './account/infrastructure/api/accounts-router';
 import kidsRouter from './kids/infrastructure/api/kids-router';
 import booksRouter from './books/infrastructure/api/books-router';
+import storiesRouter from './story/infraestructure/api/story-router';
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.use('/customers', customersRouter);
 router.use('/accounts', accountsRouter);
 router.use('/kids', kidsRouter);
 router.use('/books', booksRouter);
+router.use('/stories', storiesRouter);
 
 export default router;

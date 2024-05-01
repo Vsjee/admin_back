1f714c728c4a680da3a26d6e6f5c1cf828dbf06b
David Hernandez <davidfelipehernandez1@gmail.com>
2024-05-01 08:45:43 -0500

    refactor: story router

--- app/story/infraestructure/api/story-router.ts
+++ app/story/infraestructure/api/story-router.ts
@@ -1,12 +1,23 @@
 import express, { Request, Response } from 'express';
 import StorySchema from '../models/story.model';
-import {
-  translateFamilyContexts,
-  translatePersonalContexts,
-} from '../../../../utils/contexts_translate_util';
 
 const storyRouter = express.Router();
 
+/**
+ * [GET] stories
+ */
+storyRouter.get('/', async (req: Request, res: Response) => {
+  try {
+    const books = await StorySchema.find();
+    res.json(books);
+  } catch (error: any) {
+    res.json({ error: error.message });
+  }
+});
+
+/**
+ * [GET] storie by id
+ */
 storyRouter.get('/:id', async (req: Request, res: Response) => {
   const { id } = req.params;
 
@@ -18,51 +29,31 @@ storyRouter.get('/:id', async (req: Request, res: Response) => {
   }
 });
 
-storyRouter.post('/', async (req: Request, res: Response) => {
-  const type = req.query['type'];
+/**
+ * [PATCH] storie by id
+ */
+storyRouter.patch('/patch/:id', async (req: Request, res: Response) => {
+  const { id } = req.params;
 
   try {
-    if (type === 'mobile') {
-      let parsedFamilyContexts: string[] =
-        req.body.characters[0].family_context;
-      let parsedPersonalContexts: string[] =
-        req.body.characters[0].personal_context;
-
-      if (parsedFamilyContexts.length > 0) {
-        req.body.characters[0].family_context =
-          translateFamilyContexts(parsedFamilyContexts);
-      }
-
-      if (parsedPersonalContexts.length > 0) {
-        req.body.characters[0].personal_context = translatePersonalContexts(
-          parsedPersonalContexts
-        );
-      }
-
-      const story = await StorySchema.create(req.body);
-
-      const storyId = story.id;
-
-      const findStory = await StorySchema.findById(storyId);
-
-      res.json(findStory);
-    } else {
-      const story = await StorySchema.create(req.body);
-      res.json(story);
-    }
+    const books = await StorySchema.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.json(books);
   } catch (error: any) {
     res.json({ error: error.message });
   }
 });
 
-storyRouter.patch('/:id', async (req: Request, res: Response) => {
+/**
+ * [DELETE] storie by id
+ */
+storyRouter.delete('/delete/:id', async (req: Request, res: Response) => {
   const { id } = req.params;
 
   try {
-    const story = await StorySchema.findByIdAndUpdate(id, req.body, {
-      new: true,
-    });
-    res.json(story);
+    const books = await StorySchema.findByIdAndDelete(id);
+    res.json(books);
   } catch (error: any) {
     res.json({ error: error.message });
   }

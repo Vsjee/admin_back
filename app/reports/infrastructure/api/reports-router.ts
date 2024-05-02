5eec115dc8a1bc5df9f8ad165c11fe8d0c63013c
David Hernandez <davidfelipehernandez1@gmail.com>
2024-05-02 11:38:26 -0500

    add : customer report endpoint in reports router

--- app/reports/infrastructure/api/reports-router.ts
+++ app/reports/infrastructure/api/reports-router.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import { globalReportPDF } from '../../domain/model/global_report_pdf';
+import { customerReportPDF } from '../../domain/model/customer_report_pdf';
 
 const reportsRouter = express.Router();
 
@@ -19,6 +20,28 @@ reportsRouter.get('/global', (req: Request, res: Response) => {
     );
 
     res.send('Creating global report...');
+  } catch (error) {}
+});
+
+/**
+ * [GET] customer report
+ */
+reportsRouter.get('/customer/:id', (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const stream = res.writeHead(200, {
+      'Content-Type': 'application/pdf',
+      'Content-Disposition': 'attachment; filename=global-report.pdf',
+    });
+
+    customerReportPDF(
+      (data: any) => stream.write(data),
+      () => stream.end(),
+      id
+    );
+
+    res.send(`Creating customer ${id} report...`);
   } catch (error) {}
 });
 

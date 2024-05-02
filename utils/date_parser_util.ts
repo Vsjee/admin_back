67038fe2a01afcad45aca54cf39601d4fb6d40c0
David Hernandez <davidfelipehernandez1@gmail.com>
2024-05-02 11:09:37 -0500

    refactor: date parser format

--- utils/date_parser_util.ts
+++ utils/date_parser_util.ts
@@ -1,8 +1,8 @@
 export const date_parser_util = (date: number): string => {
   if (date.toString().length === 10) {
-    return new Date(date * 1000).toLocaleDateString();
+    return new Date(date * 1000).toLocaleString();
   } else if (date.toString().length === 13) {
-    return new Date(date).toLocaleDateString();
+    return new Date(date).toLocaleString();
   } else {
     return Date.now().toLocaleString();
   }

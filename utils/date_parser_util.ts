fbc7e5afa0ff065def1c874b3b2f2c71fe1f7457
David Hernandez <davidfelipehernandez1@gmail.com>
2024-05-02 01:02:04 -0500

    add: date parser util

---
+++ utils/date_parser_util.ts
@@ -0,0 +1,9 @@
+export const date_parser_util = (date: number): string => {
+  if (date.toString().length === 10) {
+    return new Date(date * 1000).toLocaleDateString();
+  } else if (date.toString().length === 13) {
+    return new Date(date).toLocaleDateString();
+  } else {
+    return Date.now().toLocaleString();
+  }
+};
